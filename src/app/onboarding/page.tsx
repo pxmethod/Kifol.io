86ed4f8cd12911c8727311c48463b371b8b4f7af
John Menard <jn.a.menard@gmail.com>
2026-04-16 11:50:39 -0400

    Onboarding copy updates

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -191,7 +191,7 @@ export default function OnboardingPage() {
                 Create your first portfolio
               </h1>
               <p className="text-md text-discovery-grey leading-relaxed max-w-xl">
-                You can add more portfolios later. Everything you add here will be saved after signing up and can be updated
+                This portfoliowill be saved after signing up and can be updated
                 at anytime. <Link href="/auth/signup" className="text-discovery-orange hover:underline">Click here</Link> if you're ready to sign up now. 
               </p>
             </div>
